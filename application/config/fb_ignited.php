1529f4fdf79cef563ac309261e8a8a94422425b6
alfonso <alfonso@darktemptress>
2012-10-11 16:52:56 -0500

    Fixed the fb_login, it wasn't accepting a null value.

--- application/config/fb_ignited.php
+++ application/config/fb_ignited.php
@@ -9,12 +9,14 @@
  * fb_apptype	set to either 'iframe' or 'connect' based on what platform your app is
  * 				is running on. 
  * fb_auth		is the default authentications, '' is basic authentication
+ * fb_upload	tells the SDK whether or not file uploads are enabled on your server.
  */
 $config['fb_appid']		= 'application id';
 $config['fb_secret']	= 'application secret';
 $config['fb_canvas']	= 'your-app-address-on-fb';
 $config['fb_apptype']	= 'iframe';
 $config['fb_auth']		= '';
+$config['fb_upload']	= false;
 
 /**
  * --- fb_canvas examples ---
