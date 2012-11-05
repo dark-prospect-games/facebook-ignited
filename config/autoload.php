5dcb5b7feb520d9b11fdc4cc60bebbaa5a267f6b
Alfonso Martinez <corrupted.rogues@gmail.com>
2012-11-05 00:15:58 -0600

    Fixed the clock by moving it to bottom of the page and separating it from the rest of the jQuery.

---
+++ config/autoload.php
@@ -0,0 +1,6 @@
+<?php
+# Load the birdseed config when the spark is loaded
+$autoload['config'] = array('fb_ignited');
+
+# Load the birdseed helper when the spark is loaded
+$autoload['helper'] = array('fb_ignited');
